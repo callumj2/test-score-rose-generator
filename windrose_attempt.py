534f50a4ace66b9f607dda800f17ae8f628c1f66
Callum Johnson <callum.johnson.aafc@gmail.com>
2021-10-04 11:41:49 +1100

    Initial Commit
    Rose chart and percentage changes working at this stage

---
+++ windrose_attempt.py
@@ -0,0 +1,13 @@
+from windrose import WindroseAxes
+from matplotlib import pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+
+# Create wind speed and direction variables
+
+ws = np.random.random(500) * 6
+wd = np.random.random(500) * 360
+
+ax = WindroseAxes.from_ax()
+ax.bar(wd, ws, normed=True, opening=0.8, edgecolor='white')
+ax.set_legend()
